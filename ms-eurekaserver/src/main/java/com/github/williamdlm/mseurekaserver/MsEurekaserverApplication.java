4784ee37700dd5dfc9701b4e98fb22d025b7524c
William Mota <william.mota.ext@spms.min-saude.pt>
2024-04-23 17:45:37 +0100

    feat: Created ms-eurekaserver

---
+++ ms-eurekaserver/src/main/java/com/github/williamdlm/mseurekaserver/MsEurekaserverApplication.java
@@ -0,0 +1,15 @@
+package com.github.williamdlm.mseurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class MsEurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEurekaserverApplication.class, args);
+	}
+
+}
---
+++ ms-eurekaserver/src/test/java/com/github/williamdlm/mseurekaserver/MsEurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.williamdlm.mseurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
