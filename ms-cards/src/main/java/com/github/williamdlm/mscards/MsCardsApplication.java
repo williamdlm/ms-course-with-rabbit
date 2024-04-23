db394e157fe9993a734791878d8fa8a1a2ac1dfa
William Mota <william.mota.ext@spms.min-saude.pt>
2024-04-23 22:51:04 +0100

    feat: Created ms-cards

---
+++ ms-cards/src/main/java/com/github/williamdlm/mscards/MsCardsApplication.java
@@ -0,0 +1,13 @@
+package com.github.williamdlm.mscards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCardsApplication.class, args);
+	}
+
+}
