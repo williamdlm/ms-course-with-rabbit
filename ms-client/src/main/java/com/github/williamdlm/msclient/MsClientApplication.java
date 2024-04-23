1826b44f279849901c466e3be8e5bf68bf8e0172
William Mota <william.mota.ext@spms.min-saude.pt>
2024-04-23 17:48:23 +0100

    feat: Created ms-client

---
+++ ms-client/src/main/java/com/github/williamdlm/msclient/MsClientApplication.java
@@ -0,0 +1,13 @@
+package com.github.williamdlm.msclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsClientApplication.class, args);
+	}
+
+}
---
+++ ms-client/src/test/java/com/github/williamdlm/msclient/MsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.williamdlm.msclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
