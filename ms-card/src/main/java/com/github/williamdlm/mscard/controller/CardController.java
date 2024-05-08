3501f55f5bc6aa51becb5873f95fd66ebb6b89ec
William Mota <william.mota.ext@spms.min-saude.pt>
2024-05-08 23:41:39 +0100

    feat: Created route findByDocument in CardController

--- ms-card/src/main/java/com/github/williamdlm/mscard/controller/CardController.java
+++ ms-card/src/main/java/com/github/williamdlm/mscard/controller/CardController.java
@@ -1,16 +1,19 @@
 package com.github.williamdlm.mscard.controller;
 
 import com.github.williamdlm.mscard.dto.CardDTO;
+import com.github.williamdlm.mscard.dto.ClientCardDTO;
 import com.github.williamdlm.mscard.model.Card;
+import com.github.williamdlm.mscard.model.ClientCard;
 import com.github.williamdlm.mscard.service.CardService;
+import com.github.williamdlm.mscard.service.ClientCardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/cards")
@@ -18,9 +21,10 @@ import java.util.List;
 public class CardController {
 
     private final CardService cardService;
+    private final ClientCardService clientCardService;
 
     @PostMapping
-    public ResponseEntity saveCard(@RequestBody CardDTO cardDTO){
+    public ResponseEntity saveCard(@RequestBody CardDTO cardDTO) {
         Card newCard = cardService.saveCard(cardDTO);
         URI headerLocation = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -31,16 +35,24 @@ public class CardController {
         return ResponseEntity.created(headerLocation).build();
 
     }
+
     @GetMapping("/search")
-    public ResponseEntity<List<Card>> findAllCards(){
+    public ResponseEntity<List<Card>> findAllCards() {
         List<Card> cardList = cardService.findAllCards();
         return ResponseEntity.ok(cardList);
     }
 
     @GetMapping(params = "income")
     public ResponseEntity<List<Card>> findByIncomeLessThanEqual(@RequestParam("income") Long income) throws Exception {
-        List<Card>  cardList = cardService.getCardIncomeLessThanEqual(income);
+        List<Card> cardList = cardService.getCardIncomeLessThanEqual(income);
         return ResponseEntity.ok(cardList);
+    }
+
+    @GetMapping(params = "document")
+    public ResponseEntity<List<ClientCardDTO>> findByDocument(@RequestParam("document") String document) {
+        List<ClientCard> clientCardList = clientCardService.findByDocument(document);
+        List<ClientCardDTO> resultList = clientCardList.stream().map(ClientCardDTO::fromModel).collect(Collectors.toList());
+        return ResponseEntity.ok(resultList);
     }
 
 }
