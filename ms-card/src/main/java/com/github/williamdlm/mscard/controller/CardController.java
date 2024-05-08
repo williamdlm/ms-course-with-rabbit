6db0cde4fd1bf415e9d6af1825ab843e932297f4
William Mota <william.mota.ext@spms.min-saude.pt>
2024-05-08 22:38:00 +0100

    refactor: Added route findByIncomeLessThanEqual

--- ms-card/src/main/java/com/github/williamdlm/mscard/controller/CardController.java
+++ ms-card/src/main/java/com/github/williamdlm/mscard/controller/CardController.java
@@ -4,6 +4,7 @@ import com.github.williamdlm.mscard.dto.CardDTO;
 import com.github.williamdlm.mscard.model.Card;
 import com.github.williamdlm.mscard.service.CardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -26,12 +27,19 @@ public class CardController {
                 .query("cardNumber={cardNumber}")
                 .buildAndExpand(cardDTO.cardNumber())
                 .toUri();
+//        return ResponseEntity.status(HttpStatus.CREATED).build(); //Alternative way
         return ResponseEntity.created(headerLocation).build();
 
     }
-    @GetMapping
+    @GetMapping("/search")
     public ResponseEntity<List<Card>> findAllCards(){
         List<Card> cardList = cardService.findAllCards();
+        return ResponseEntity.ok(cardList);
+    }
+
+    @GetMapping(params = "income")
+    public ResponseEntity<List<Card>> findByIncomeLessThanEqual(@RequestParam("income") Long income) throws Exception {
+        List<Card>  cardList = cardService.getCardIncomeLessThanEqual(income);
         return ResponseEntity.ok(cardList);
     }
 
