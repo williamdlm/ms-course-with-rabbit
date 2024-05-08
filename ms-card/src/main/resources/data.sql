1a2a19fd929e21ad3d19712603adbd53993ce9d3
William Mota <william.mota.ext@spms.min-saude.pt>
2024-05-08 23:40:14 +0100

    feat: Added initial data

---
+++ ms-card/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO card (name, card_number, income, card_limit, card_brand)VALUES ('MasterCard GOLD', '0002 3000 2005 2024', 5000.0, 10000, 'MASTER');
---
+++ ms-client/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+-- Cliente 1
+INSERT INTO client (name, id_document)VALUES ('João da Silva', '12345678900');
+
+-- Cliente 2
+INSERT INTO client (name, id_document)VALUES ('Maria Oliveira', '98765432100');
+
+-- Cliente 3
+INSERT INTO client (name, id_document)VALUES ('Pedro Souza', '45678912300');
