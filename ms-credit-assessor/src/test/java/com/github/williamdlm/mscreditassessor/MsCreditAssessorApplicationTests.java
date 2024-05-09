26949486d4b4ae24d46e722fa49176326a646a6b
William Mota <william.mota.ext@spms.min-saude.pt>
2024-05-09 09:39:51 +0100

    feat: Created project MsCreditAssessor

---
+++ ms-credit-assessor/src/test/java/com/github/williamdlm/mscreditassessor/MsCreditAssessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.williamdlm.mscreditassessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCreditAssessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
