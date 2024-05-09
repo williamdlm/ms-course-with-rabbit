ef3533afa5ed0c2c8fede30667473a78f5735fef
William Mota <william.mota.ext@spms.min-saude.pt>
2024-05-10 00:26:30 +0100

    feat: Added subscriber in ms-card with rabbitmq

--- ms-card/src/main/java/com/github/williamdlm/mscard/MsCardApplication.java
+++ ms-card/src/main/java/com/github/williamdlm/mscard/MsCardApplication.java
@@ -1,9 +1,11 @@
 package com.github.williamdlm.mscard;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class MsCardApplication {
 
 	public static void main(String[] args) {
